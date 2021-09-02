56fffcfcf158922374a098be08bff32a3d8a2c72
Erick Solis Mahl <A00904052@itesm.mx>
2021-09-01 17:13:02 -0700

    TEST-1234 Added connectivity to end point to convert temperatures

--- convertidorTemperatura/ViewController.swift
+++ convertidorTemperatura/ViewController.swift
@@ -8,15 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let temperatureConverter = TemperatureConverter()
+    let temperatureConverterService = TemperatureConverterService()
     @IBOutlet weak var celsiusTextField: UITextField!
     @IBOutlet weak var fahrenheitTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func convertToFarenheit(_sender: UIButton) {
+    @IBAction func convertir(_sender: UIButton) {
+        fahrenheitTextField.text = ""
+                
+        if let celciusValue = celsiusTextField.text {
+            if !celciusValue.isEmpty {
+                /*let fahrenheitValue = temperatureConverter.convert(temperature: Temperature(value: Float16(celciusValue)!, unit: Temperature.Unit.CELSIUS), unitToConvert: Temperature.Unit.FAHRENHEIT)
+                print("Farenheit" + String(fahrenheitValue.value))
+                fahrenheitTextField.text = String(fahrenheitValue.value)*/
+                
+                temperatureConverterService.convertToFahrenheit(temperature: Temperature(value: Float16(celciusValue)!, unit: Temperature.Unit.CELSIUS))  {
+                    [weak self] (fahrenheitTemperature) in
+                            DispatchQueue.main.async {
+                                self?.fahrenheitTextField.text = String(fahrenheitTemperature.value)
+                            }
+                                    
+                }
+                
+            } else {
+                print("Celcius value is empty")
+            }
+                
+        }
+        
     }
     
 }
